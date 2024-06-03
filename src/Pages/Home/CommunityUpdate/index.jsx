a6a691c8a1a809b3fded0393b624bca53f305ced
Sammyalade <olufemialadeyelu@gmail.com>
2024-06-03 15:32:40 +0100

    react

--- src/Pages/Home/CommunityUpdate/index.jsx
+++ src/Pages/Home/CommunityUpdate/index.jsx
@@ -6,9 +6,9 @@ const CommunityUpdate = ()=>{
             <div>
                 <div className={styles.community}>
                     <h1>Caring is the new marketing</h1>
-                    <p style={{textAlign: "center"}}>The Nexcent blog is the best place to read about the latest membership
-                        insights,<br/>trends and more. See who's joining the community, read about how our
-                        community<br/>are increasing their membership income and lot's more.</p>
+                    <p>The Nexcent blog is the best place to read about the latest membership
+                        insights, trends and more. See who's joining the community, read about how our
+                        community are increasing their membership income and lot's more.</p>
                 </div>
             </div>
         </>
