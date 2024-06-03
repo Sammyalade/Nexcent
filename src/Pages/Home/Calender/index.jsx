dc077cde4c7aa7f936ed48eeba75ce1bbe6aac58
Sammyalade <olufemialadeyelu@gmail.com>
2024-06-03 15:32:13 +0100

    react

--- src/Pages/Home/Calender/index.jsx
+++ src/Pages/Home/Calender/index.jsx
@@ -11,12 +11,12 @@ const Calender = ()=>{
                         <h1>How to design your site footer like<span style={{display: "block"}}>we did</span></h1>
                     </div>
                     <div>
-                        <p>Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt<br/>molestie,
-                            massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at<br/>libero
-                            ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta<br/>nisi
+                        <p style={{paddingRight: "100px"}}>Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie,
+                            massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero
+                            ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi
                             facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt.
-                            Integer<br/>in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est,
-                            finibus et nisi<br/>ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id
+                            Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est,
+                            finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id
                             gravida.</p>
                     </div>
                     <div>
