fbc62f023153e2dcbbb92a74aa6df50d3db0015f
Sammyalade <olufemialadeyelu@gmail.com>
2024-06-03 15:33:59 +0100

    react

--- src/Pages/Home/MeetCustomer/Content/index.jsx
+++ src/Pages/Home/MeetCustomer/Content/index.jsx
@@ -5,13 +5,13 @@ const Content = ()=>{
     return(
         <>
             <div>
-                <p>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum
-                    risus<br/>tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam
-                    in, gravida<br/>enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula
-                    molestie, nec molestie<br/>mi blandit. Suspendisse cursus tellus sed augue ultrices, quis
-                    tristique nulla sodales. Suspendisse<br/>eget lorem eu turpis vestibulum pretium. Suspendisse
-                    potenti. Quisque malesuada enim sapien,<br/>vitae placerat ante feugiat eget. Quisque vulputate
-                    odio neque, eget efficitur libero condimentum<br/>id. Curabitur id nibh id sem dignissim finibus
+                <p style={{width: "90%"}}>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum
+                    risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam
+                    in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula
+                    molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis
+                    tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse
+                    potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate
+                    odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus
                     ac sit amet magna.</p>
                 <Tim/>
                 <Logo/>
